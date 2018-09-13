3912ce8e09d8bb7fa099196a4599ac08a7c78a86
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2018-09-14 07:59:05 +0900

    fix 109_2

--- 109_2/main.cpp
+++ 109_2/main.cpp
@@ -3,9 +3,28 @@
 using namespace std;
 
 int main(void) {
-    int a, b;
-    cin >> a >> b;
+    int n;
+    string words[1000]; // since max is 1000
+    bool badWord = false;
 
-    cout << "ok";
+    cin >> n;
+    for (int i =0; i < n; ++i) {
+        cin >> words[i];
+        if (!badWord && i > 0 && words[i - 1].back() != words[i].front()) {
+            badWord = true; // last word last char is not same as new word first char
+        }
+        for (int j = i - 1; !badWord && j > 0; --j) {
+            if (words[i] == words[j]) {
+                badWord = true;  // word is already said
+                break;
+            }
+        }
+    }
+
+    if (badWord) {
+        cout << "No";
+    } else {
+        cout << "Yes";
+    }
     return 0;
 }
