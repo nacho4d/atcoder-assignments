02c38cbbe84769359be82dd091b005d517d36fc7
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2018-09-14 09:00:59 +0900

    fix 109_2

--- 109_2/main.cpp
+++ 109_2/main.cpp
@@ -8,12 +8,12 @@ int main(void) {
     bool badWord = false;
 
     cin >> n;
-    for (int i =0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         cin >> words[i];
         if (!badWord && i > 0 && words[i - 1].back() != words[i].front()) {
             badWord = true; // last word last char is not same as new word first char
         }
-        for (int j = i - 1; !badWord && j > 0; --j) {
+        for (int j = i - 1; !badWord && j >= 0; --j) {
             if (words[i] == words[j]) {
                 badWord = true;  // word is already said
                 break;
